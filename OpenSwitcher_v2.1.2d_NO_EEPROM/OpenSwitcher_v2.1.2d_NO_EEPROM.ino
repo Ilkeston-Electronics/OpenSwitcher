91f72e59ee7eb8af6d71ab5762ba4a64ed1f3be8
Ilkeston-Electronics <89555920+Ilkeston-Electronics@users.noreply.github.com>
2021-09-27 13:57:22 +0100

    double oops

--- OpenSwitcher_v2.1.2d_NO_EEPROM/OpenSwitcher_v2.1.2d_NO_EEPROM.ino
+++ OpenSwitcher_v2.1.2d_NO_EEPROM/OpenSwitcher_v2.1.2d_NO_EEPROM.ino
@@ -13,7 +13,8 @@ Adafruit_DotStar strip = Adafruit_DotStar(1, INTERNAL_DS_DATA, INTERNAL_DS_CLK, 
 unsigned long ticktimer1;
 unsigned long ticktimer2;
 static bool swapactive;
-static bool relaylatching;
+static bool latchingrelay;
+static bool relaychecked;
 static int usertimer;
 
 void led_show(const int red, const int green, const int blue)
@@ -41,49 +42,33 @@ bool swap_drives()
   return swapactive;
 }
 
-bool set_relay()
+bool set_relay(const bool swap, const bool relaytype)
 {
-  static bool coil1_active = false;
-  static bool coil2_active = false;
-  if (swapactive)
+  if (relaytype)
+  {
+    if (swap)
     {
-
-      if (relaylatching)
-      {
-        if (!coil1_active)
-        {
-          digitalWrite(relaypin1, HIGH); delayMicroseconds (500000); digitalWrite(relaypin1, LOW);
-          coil1_active = true;
-          coil2_active = false;
-        }
-      }
-      else
-      {
-        digitalWrite(relaypin1, HIGH);
-        coil1_active = true;
-        coil2_active = false;
-      }
-      
+      digitalWrite(relaypin1, HIGH); delayMicroseconds (500000); digitalWrite(relaypin1, LOW); return 1;
     }
     else
     {
-      if (relaylatching)
-      {
-        if (!coil2_active)
-        {
-          digitalWrite(relaypin2, HIGH); delayMicroseconds (500000); digitalWrite(relaypin2, LOW);
-          coil2_active = true;
-          coil1_active = false;
-        }
-      }
-      else
-      {
-        digitalWrite(relaypin1, LOW);
-        coil1_active = false;
-        coil2_active = false;
-      }
+      digitalWrite(relaypin2, HIGH); delayMicroseconds (500000); digitalWrite(relaypin2, LOW); return 0;
     }
-    return coil1_active;
+  }
+  else
+  {
+    if (swap)
+    {
+      digitalWrite(relaypin1, HIGH);
+      return 1;
+    }
+    else
+    {
+      digitalWrite(relaypin1, LOW);
+      return 0;
+    }
+
+  }
 }
 
 void first_boot_lock()
@@ -104,57 +89,65 @@ int check_for_reset()
 
     if ((ticktimer2 - ticktimer1) == (usertimer * 1000) && (digitalRead(switch_request) == HIGH))
     {
-        led_flash (64, 64, 64, 3, 50000,316000);
-        if (digitalRead(switch_request) == LOW) return 1;
+      led_flash (64, 64, 64, 3, 50000, 316000);
+      if (digitalRead(switch_request) == LOW) return 1;
     }
-    
+
   }
   //if(res) ticktimer1 = millis();
   return 0;
 }
 
+bool relaylatching()
+{
+  bool res = false;
+  pinMode(relaypin2, INPUT_PULLUP);
+  if (digitalRead(relaypin2) == LOW)
+  {
+    res = true;
+    //led_show(0, 8, 8);
+    pinMode(relaypin2, OUTPUT);
+  }
+  return res;
+}
+
 void setup() {
- 
+
   //lock system if not plugged in to Amiga. Link keyboard reset and 5v to bypass. Set reset sense pin.
 
   first_boot_lock();
-pinMode(relaypin2, INPUT_PULLUP);
-    if (digitalRead(relaypin2) != HIGH) relaylatching = true;
-    else relaylatching = false;
+
   //set other pins
 
   pinMode(switch_request, INPUT_PULLUP);
   pinMode(cia_sel1, INPUT_PULLUP);
   pinMode(relaypin1, OUTPUT);
-
-  //Reset relay into default position if latching
-  if (relaylatching)
-  {
-    pinMode(relaypin2, OUTPUT);
-    digitalWrite(relaypin2, HIGH); delayMicroseconds (100000); digitalWrite(relaypin2, LOW);
-  }
-  else pinMode(relaypin2, INPUT_PULLUP);
   swapactive = START_MODE;
   usertimer = (USER_TIMER_SETUP - 1);
+  relaychecked = false;
   led_show(0, 0, 0);
 }
 
 void loop() {
   //ticktimer1 = millis();
- 
-  
-  if (relaylatching)
+  if(!relaychecked)
+  {
+  latchingrelay = relaylatching();
+  relaychecked = true;
+  }
+  if (latchingrelay)
   {
     digitalWrite(relaypin2, HIGH); delayMicroseconds (100000); digitalWrite(relaypin2, LOW);
   }
-  
+
   while (digitalRead(cia_sel0) == LOW);
   delayMicroseconds(690);
-  set_relay();
-  if (swapactive) led_show(0, 0, 8); 
-  else led_show(0, 8, 0);
   
-  while (digitalRead(switch_request) == LOW);
+  if (set_relay(swapactive, latchingrelay)) led_show(0, 0, 8);
+  else led_show(0, 8, 0);
+  delay(500);
   led_show(0, 0, 0);
-    if (check_for_reset() == 1) swap_drives();
-  }
+  while (digitalRead(switch_request) == LOW);
+  
+  if (check_for_reset() == 1) swap_drives();
+}
